722939b18a6056864f5374152d4ea412c90a0267
dwlee <zckixw11@gmail.com>
2023-06-09 17:54:52 +0900

    add description

--- Menu/Menu.java
+++ Menu/Menu.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 /*
  * template method pattern
+ * 특정 작업을 처리하는 일부분을 서브클래스로 캡슐화
  */
 public abstract class Menu {
 
@@ -29,6 +30,8 @@ public abstract class Menu {
         this.priority = priority;
     }
 
+    // 공통적으로 처리되는 특정작업인 template 메서드
+    // 모든 메뉴는 해당 메소드가 실행되어 메뉴에 대한 처리를 하지만 구체적인 작업은 각 메뉴마다 다를 수있다.
     public Menu operation(Scanner sc){
 
         Menu    rtnMenu = null;
@@ -51,6 +54,7 @@ public abstract class Menu {
         return rtnMenu;
     }
 
+    // 서브클래스에서 구현될 hook 메소드
     public abstract Menu exec(String s);
     public abstract void print();
 
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ design pattern 이해를 위한 메뉴관련 예시 프로그램
 - composite
 - template method
 - simple factory
+
+with java
